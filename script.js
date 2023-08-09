f10aa304a236d4430f55df9622de74660fd1f1b8
Ankit Yande <ankityande@gmail.com>
2023-08-08 23:11:13 -0500

    add and edit text

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ current = '#current'
 
 $(document).ready(() => {
 
-    $("#add").on("click", () => {
+    $("#addDiv").on("click", () => {
         $(current).append(
-            "<div>Test</div>"
+            "<div></div>"
+        )
+    });
+    $("#addTxt").on("click", () => {
+        $(current).append(
+            "<p>Text Here</p>"
+        )
+    });
+
+    $("#delete").on("click", () => {
+        if (current != $("#container")) //fix this
+            $(current).remove()
+    });
+
+    $("#cngTxt").on("click", () => {
+        newText = $("#text").val()
+        $(current).text(
+            `${newText}`
         )
     });
 
@@ -43,27 +60,72 @@ $(document).ready(() => {
         });
     });
 
-    $("#bg").change(function(){
-        $(current).css("background-color",$("#bg").val());
+    $("#bg").change(function () {
+        $(current).css("background-color", $("#bg").val());
     });
-    $("#fg").change(function(){
-        $(current).css("color",$("#fg").val());
+    $("#fg").change(function () {
+        $(current).css("color", $("#fg").val());
     });
 
-    $("#parent").children().mouseover(function(e){
-        $(".highlight").removeClass("highlight");     
+    $("#parent").children().mouseover(function (e) {
+        $(".highlight").removeClass("highlight");
         $(e.target).addClass("highlight");
-      return false;
-    }).mouseout(function(e) {
+        return false;
+    }).mouseout(function (e) {
         $(this).removeClass("highlight");
     });
 
-    $("#parent").children().click(function(e){
+    $("#parent").children().click(function (e) {
         current = e.target
-        $(".select").removeClass("select");     
+        $(".select").removeClass("select");
         $(e.target).addClass("select");
-      return false;
+        changeVisibleSettings()
+        return false;
     });
 
+    $("#download").on("click", () => {
+        var a = document.createElement('a');
+        // serialize the whole document as a string
+        var doc = new XMLSerializer().serializeToString($("#parent")[0]);
+        // convert this string to a blob object
+        if (window.Blob) {
+            var blob = new Blob([doc], { type: 'text/html' });
+            // create a blob URL
+            a.href = URL.createObjectURL(blob);
+        } else {
+            //browser don't support Blob object, create a data url
+            a.href = 'data: text/html; charset=utf8, ' + doc;
+        }
+
+        // The following won't work in the snippet but can be used on your server for browsers supporting download attribute
+
+        if ('download' in a) {
+            a.download = 'yourPageName.html';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        } else {
+            a.innerHTML = 'Right click - Save As.. to download the page';
+            document.body.appendChild(a);
+        }
+    })
+
+    $("#log").on("click", () => {
+        console.log(current.tagName)
+    })
+
 });
+
+changeVisibleSettings = () =>{
+    $("#left-panel").children().hide(); 
+    if(current.tagName == "DIV"){
+        $("#divTags").show();
+    }
+    else if(current.tagName == "P"){
+        $("#textTags").show();
+    }
+    $("#constantTags").show();
+}
+
+
 
